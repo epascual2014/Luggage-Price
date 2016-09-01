928d28b0c263d6bb1aa6fbce03b28a24e716d3de
edrick <edrickepascual@gmail.com>
2016-09-01 16:44:07 -0700

    commit files

---
+++ Bag Fare/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  Bag Fare
+//
+//  Created by Edrick Pascual on 8/28/16.
+//  Copyright © 2016 Edge Designs. All rights reserved.
+//
+
+import UIKit
+
+
+class MainViewController: UIViewController {
+
+    // Add Airline Dictionary
+    // Add number of bags
+    // Add Size of bags
+    // Button
+    // Result of selection
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
